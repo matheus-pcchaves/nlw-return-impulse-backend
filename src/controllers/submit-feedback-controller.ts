0376f7b4bf708cecc47d3d2e5b1237c63ea0725a
Matheus <vicde2@hotmail.com>
2022-06-06 09:41:34 -0300

    controllers & routes

---
+++ src/controllers/submit-feedback-controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express"
+import { NodemailerMailAdapter } from "../adapters/nodemailer/nodemailer-mail-adapter"
+import { PrismaFeedbackRepository } from "../repositories/prisma/prisma-feedback-repository"
+import { SubmitFeedbackUsecase } from "../use-cases/submit-feedback-use-case"
+
+export class SubmitFeedbackController{
+    static async handle(request: Request, response: Response){
+        const { type, comment, screenshot} = request.body
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
+
+        const prismaFeedbackRepository = new PrismaFeedbackRepository()
+        const submitFeedbackUsecase = new SubmitFeedbackUsecase(
+            prismaFeedbackRepository,
+            nodemailerMailAdapter
+        )  
+
+        await submitFeedbackUsecase.execute({
+            type,
+            comment, 
+            screenshot,
+        })
+
+        return response.status(201).send('Feedback enviado com sucesso')
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,6 @@
 import express from 'express'
-import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
-import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedback-repository';
-import { SubmitFeedbackUsecase } from './use-cases/submit-feedback-use-case';
+import { SubmitFeedbackController } from './controllers/submit-feedback-controller' 
 
 export const routes = express.Router()
 
-routes.post('/feedbacks', async(req, res) =>{
-    const { type, comment, screenshot} = req.body
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-
-    const prismaFeedbackRepository = new PrismaFeedbackRepository()
-    const submitFeedbackUsecase = new SubmitFeedbackUsecase(
-        prismaFeedbackRepository,
-        nodemailerMailAdapter
-    )  
-
-    await submitFeedbackUsecase.execute({
-        type,
-        comment, 
-        screenshot,
-    })
-
-    return res.status(201).send()
-})
+routes.post('/feedbacks', SubmitFeedbackController.handle)
